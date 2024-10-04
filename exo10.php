d42161dd6da6b66995b57b76c2f03bcb35bae704
Erwin <erwin.philip68@gmail.com>
2024-10-04 16:57:06 +0200

    PHP

---
+++ exo10.php
@@ -0,0 +1,15 @@
+<h1>Exercice 10</h1>
+
+<?php
+
+$price = 152;
+$payment = 200;
+$coins = [1, 2, 5, 10]
+$currency = 48;
+
+
+
+echo "Montant à payer : $price € <br>";
+echo "Montant versé : $payment € <br>";
+echo "Reste à payer : $currency € <br>";
+
